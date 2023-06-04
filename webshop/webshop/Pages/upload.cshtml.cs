fba8ef1054fd8bec31aa1e09bc29c29d7eba9d33
Valentino Andric <63399446+vnandric@users.noreply.github.com>
2023-06-04 15:34:54 +0200

    upload almost done
    its done but not working

--- webshop/webshop/Pages/upload.cshtml.cs
+++ webshop/webshop/Pages/upload.cshtml.cs
@@ -14,14 +14,69 @@ namespace webshop.Pages
         private readonly string _uploadFolderPath;
 
         [BindProperty]
-        public IFormFileCollection Img { get; set; }
+        public IFormFileCollection UploadedFiles { get; set; }
 
         [BindProperty]
-        [Required(ErrorMessage = "The Evenement field is required.")]
-
+        [Required(ErrorMessage = "Fill in the fields!")]
         public string PT_naam { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Fill in the fields!")]
+        public string Prijs { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Fill in the fields!")]
+        public string Naam { get; set; }
 
         public string ErrorMessage { get; set; }
 
+        public uploadModel(IWebHostEnvironment webHostEnvironment)
+        {
+            _uploadFolderPath = Path.Combine(webHostEnvironment.WebRootPath, "uploads");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (UploadedFiles != null && UploadedFiles.Count > 0)
+            {
+                try
+                {
+                    using (var connection = new SqliteConnection($"Data Source={_dbPath}"))
+                    {
+                        connection.Open();
+
+                        foreach (var file in UploadedFiles)
+                        {
+                            string image = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+                            string filePath = Path.Combine("uploads", image);
+                            string physicalPath = Path.Combine(_uploadFolderPath, image);
+
+                            using (var fileStream = new FileStream(physicalPath, FileMode.Create))
+                            {
+                                await file.CopyToAsync(fileStream);
+                            }
+
+                            var insertCommand = connection.CreateCommand();
+                            insertCommand.CommandText = "INSERT INTO product (ID, PT_naam, prijs, img, naam, FilePath) VALUES (@ID, @PT_naam, @prijs, @img, @naam, @FilePath)";
+                            insertCommand.Parameters.AddWithValue("@ID", null);
+                            insertCommand.Parameters.AddWithValue("@PT_naam", PT_naam);
+                            insertCommand.Parameters.AddWithValue("@prijs", Prijs);
+                            insertCommand.Parameters.AddWithValue("@img", file.FileName);
+                            insertCommand.Parameters.AddWithValue("@naam", Naam);
+                            insertCommand.Parameters.AddWithValue("@FilePath", filePath);
+                            insertCommand.ExecuteNonQuery();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ErrorMessage = $"An error occurred while uploading files: {ex.Message}";
+                    return Page();
+                }
+          }
+
+            return RedirectToPage("Index");
+        }
     }
 }
